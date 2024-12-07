39ff678acffc446adb76268771a3311750bb42cd
Aryan <chaudharyaryan3456@gmail.com>
2024-12-07 22:33:13 +0530

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+let students = {
+    name: "Aryan", 
+    age: 23,
+    marks: 95
+};
+
+let student2 = ["Aryan", 23, 95];
+
+console.log(students);
+console.log(student2);
+
+const item = {
+    price: 100.99,
+    discount: 50,
+    colors: ["red", "pink"],
+};
+
+console.log(item);
