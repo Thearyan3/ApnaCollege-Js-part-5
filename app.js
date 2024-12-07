9d5f2c1f819fea3fe74fc8a731915cdef9386c9b
Aryan <chaudharyaryan3456@gmail.com>
2024-12-07 22:48:05 +0530

    update

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
-let students = {
-    name: "Aryan", 
-    age: 23,
-    marks: 95
-};
+// let students = {
+//     name: "Aryan", 
+//     age: 23,
+//     marks: 95
+// };
 
-let student2 = ["Aryan", 23, 95];
+// let student2 = ["Aryan", 23, 95];
 
-console.log(students);
-console.log(student2);
+// console.log(students);
+// console.log(student2);
 
-const item = {
-    price: 100.99,
-    discount: 50,
-    colors: ["red", "pink"],
-};
+// const item = {
+//     price: 100.99,
+//     discount: 50,
+//     colors: ["red", "pink"],
+// };
 
-console.log(item);
+// console.log(item);
 
-// Task --> Thread/Twitter Post 
-// Create an object literal for the properties of thread/twitter post which includes- (i).username (ii).content (iii). likes (iv). reposts (v). tags
+// // Task --> Thread/Twitter Post 
+// // Create an object literal for the properties of thread/twitter post which includes- (i).username (ii).content (iii). likes (iv). reposts (v). tags
 
 
-const post = {
-    username: "Aryan",
-    content: "This is my #firstPost",
-    likes: 15,
-    reposts: 2,
-    tags: ["@aryan2", "@delta"]
-};
-console.log(post);
+// const post = {
+//     username: "Aryan",
+//     content: "This is my #firstPost",
+//     likes: 15,
+//     reposts: 2,
+//     tags: ["@aryan2", "@delta"]
+// };
+// console.log(post);
 
-//Get Values 
-let student = {
-    name: "shradha",
-    marks: 95
+// //Get Values 
+// let student = {
+//     name: "shradha",
+//     marks: 95
+// };
+
+const obj = {
+    1: "a",
+    2: "b",
+    true: "c",
+    null: "d",
+    undefined: "e"
 };
