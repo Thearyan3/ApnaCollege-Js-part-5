26623346706f87df30cd3bdc59d4cedc6e90d02b
Aryan <chaudharyaryan3456@gmail.com>
2024-12-08 20:40:10 +0530

    created Guessing game

--- guessgame.js
+++ guessgame.js
@@ -1,4 +1,6 @@
 // Game - User enters a max number & then tries to guess a random generated number between 1 to max. 
 
 const max = prompt("Enter a maximum Number :");
-console.log(max);
+
+const random = Math.floor(Math.random() * max) + 1;
+
