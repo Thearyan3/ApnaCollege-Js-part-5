cf8f1e2107205e6ca463b3f46640675b01b20472
Aryan <chaudharyaryan3456@gmail.com>
2024-12-08 21:20:23 +0530

    Assignment questions

---
+++ Assignment.js
@@ -0,0 +1,7 @@
+// Qs 1 . Create a program that generates a random number representing a dice roll. [The number should be between 1 and 6].
+let diceRoll = Math.floor(Math.random()*6)+1;
+console.log(diceRoll);
+
+// Qs 2 . Create an object representing a car that stores the following properties for the car : name, model, color. Print the car’s name.
+
+// Qs 3 . Create an object Person with their name, age and city. Edit their city’s original value to change it to “NewYork”.Add a new property country and set it to the United States.
